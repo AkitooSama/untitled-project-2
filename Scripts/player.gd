7101114a79e955401d9254bb87775409fcbb4c18
AkitooSama <akitoomusic@gmail.com>
2025-02-18 19:07:18 +0530

    made base placeholder game

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,45 +1,38 @@
 extends CharacterBody2D
 
-@export var Deebugger_Detecter: Area2D
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 100.0
+const JUMP_VELOCITY = -300.0
 
-var DeebugObjList = []
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var jump_sound: AudioStreamPlayer2D = $SFX/JumpSound
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	# Handles jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump_sound.play()
 
-	if Input.is_action_just_pressed("Attack"):
-		pass
-		#>play animation that enables hitbox, then disables it at the end
-
-	if Input.is_action_just_pressed("Deebug"):
-		print(DeebugObjList)
-
-	# Gets the input direction and handle the movement/deceleration.
 	var direction := Input.get_axis("Left", "Right")
+	
+	if direction > 0:
+		animated_sprite_2d.flip_h = false
+	elif direction < 0:
+		animated_sprite_2d.flip_h = true
+		
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite_2d.play("idle")
+		else:
+			animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("jump")
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
-
-func take_damage(amount: int) -> void:
-	print("Damage: ", amount)
-	#play death anim (queue_free() at end of death anim)
-	queue_free()
-
-
-func _on_deebugger_range_area_entered(area: Area2D) -> void:
-	DeebugObjList.append(area)
-
-
-func _on_deebugger_range_area_exited(area: Area2D) -> void:
-	DeebugObjList.erase(area)
